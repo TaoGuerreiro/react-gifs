271cf7ce7c3044d6bc216efba8d6cf8bb53c67a8
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-03-31 12:05:37 +0200

    ok

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'React';
 
 
 class Gif extends Component {
+  constructor(props) { // be promoted into a class
+    super(props);
+    this.state = { clicked: false }; // defines initial state
+  }
+
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
   render () {
     const src=`https://media1.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
-        <img src={src} alt="" className="gif"/>
+        <img src={src} alt="" className={`gif ${this.state.clicked ? "clicked" : ""}`}
+         onClick={this.handleClick}/>
       );
   }
 }
